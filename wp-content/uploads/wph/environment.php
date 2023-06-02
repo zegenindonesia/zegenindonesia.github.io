480b8b426270381dcf366722a2571d61812a4e40
zegenindonesia <zegenutamaweb@gmail.com>
2023-06-03 05:04:08 +0700

    Signed-off-by: zegenindonesia <zegenutamaweb@gmail.com>

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"madxflow","mapped_name":"layout"},"allowed_paths":["\/home\/zegenuta\/public_html\/wp-content\/themes"],"cache_path":"\/home\/zegenuta\/public_html\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/"}'; ?>
